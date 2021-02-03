0d1f297240e51d0979eed0b47e6a6334519e925d
uthircloudnative <uthircloudnative@gmail.com>
2021-02-02 19:11:12 -0500

    Updated Test class to expect Json

--- src/test/java/com/galvanize/opentrivia/it/controller/TriviaControllerTest.java
+++ src/test/java/com/galvanize/opentrivia/it/controller/TriviaControllerTest.java
@@ -1,12 +1,18 @@
 package com.galvanize.opentrivia.it.controller;
 
+import org.h2.util.json.JSONString;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -21,6 +27,9 @@ public class TriviaControllerTest {
     void getTriviaQuestions_returnsEmptyQuestions200() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/triviaqustions"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(0)));
+
+
     }
 }
